6bb670bba0d1c55b07cf0116c0244ed56cae342a
Your Name <you@example.com>
2018-05-12 14:42:40 +0400

    world

---
+++ script/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+    var content = $("<div/>").css({
+        width: "85vw",
+        height: "98vh",
+        margin: "auto",
+        border: "1px solid black",
+    }).appendTo("body");
+
+    var world = $("<div/>").css({
+        width: "85vw",
+        height: "78vh",
+        border: "1px solid black",
+        position: "relative",
+    }).appendTo(content);
+
+    var building = $("<div/>").css({
+        width: "72vw",
+        height: "78vh",
+        margin: "auto",
+        border: "1px solid black",
+        backgroundColor: "#663300",
+        position: "relative",
+    }).appendTo(world);
+
+    var roof1 = $("<div/>").css({
+        width: "71.8vw",
+        height: "27vh",
+        border: "1px solid",
+        position:"relative",
+        top:"50.8vh",
+    }).attr("class", "roof1 roof").appendTo(building);
+
+    var roof2 = $("<div/>").css({
+        width: "71.8vw",
+        height: "27vh",
+        border: "1px solid",
+        position:"relative",
+        bottom:"3.5vh",
+    }).attr("class", "roof2 roof").appendTo(building);
+
+    var door = $("<div/>").css({
+        width: "22vw",
+        height: "27vh",
+        margin: "auto",
+        border: "1px solid black",
+        position: "relative",
+    }).attr("class", "door").appendTo(roof1);
+
+
+    function windows(tOp, riGht,boTTom,leFt,app,name){
+        var name = $("<div/>").css({
+            width: "10vw",
+            height: "14vh",
+            border: "1px solid black",
+            position: "relative",
+            top:tOp,
+            right:riGht,
+            bottom:boTTom,
+            left:leFt,
+        }).attr("class", "window").appendTo(app);
+    }
+    
+    windows("2vh","0px","0px","5vw",roof2,window1);
+
+    var window1 = $("<div/>").css({
+        width: "10vw",
+        height: "14vh",
+        border: "1px solid black",
+        position: "relative",
+       left:""
+    }).attr("class", "window").appendTo(roof2);
+
+    var route = $("<div/>").css({
+        height: "19.5vh",
+        border: "1px solid black",
+    }).attr("id", "route").appendTo(content);
+})
