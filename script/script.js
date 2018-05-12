946616640631a78175cf3d00ac89318f88355880
Your Name <you@example.com>
2018-05-12 16:24:53 +0400

    roof

--- script/script.js
+++ script/script.js
@@ -22,21 +22,35 @@ $(document).ready(function () {
         position: "relative",
     }).appendTo(world);
 
-    var roof1 = $("<div/>").css({
-        width: "71.8vw",
-        height: "27vh",
-        border: "1px solid",
-        position:"relative",
-        top:"50.8vh",
-    }).attr("class", "roof1 roof").appendTo(building);
 
-    var roof2 = $("<div/>").css({
-        width: "71.8vw",
-        height: "27vh",
-        border: "1px solid",
-        position:"relative",
-        bottom:"3.5vh",
-    }).attr("class", "roof2 roof").appendTo(building);
+
+    function roof(tOp, app, name) {
+        var name = $("<div/>").css({
+            width: "71.8vw",
+            height: "27vh",
+            border: "1px solid",
+            position: "relative",
+            top: tOp,
+        }).attr("class", "roof1 roof").appendTo(app);
+    }
+
+    roof("50.8vh", building, "roof1");
+    function windows(tOp, riGht, boTTom, leFt, app, name) {
+        var name = $("<div/>").css({
+            width: "10vw",
+            height: "14vh",
+            border: "1px solid black",
+            position: "relative",
+            display: "inline-block",
+            top: tOp,
+            right: riGht,
+            bottom: boTTom,
+            left: leFt,
+        }).appendTo(app);
+    }
+    for (var i = 5; i < 27; i += 7) {
+        windows("5vh", "0px", "0px", i + "vw", roof2, "window1");
+    }
 
     var door = $("<div/>").css({
         width: "22vw",
@@ -45,26 +59,6 @@ $(document).ready(function () {
         border: "1px solid black",
         position: "relative",
     }).attr("class", "door").appendTo(roof1);
-
-
-    function windows(tOp, riGht,boTTom,leFt,app,name){
-        var name = $("<div/>").css({
-            width: "10vw",
-            height: "14vh",
-            border: "1px solid black",
-            position: "relative",
-            display:"inline-block",
-            top:tOp,
-            right:riGht,
-            bottom:boTTom,
-            left:leFt,
-        }).appendTo(app);
-    }
-    for(var i = 5;i<27;i+=7){
-        windows("5vh","0px","0px",i+"vw",roof2,"window1");
-    }
-
-    
 
     var route = $("<div/>").css({
         height: "19.5vh",
