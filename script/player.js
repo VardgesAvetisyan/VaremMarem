b5ed99f6b276f050074d751101c413eb2a99c72b
Your Name <you@example.com>
2018-05-17 18:11:33 +0400

    world is moving with player

--- script/player.js
+++ script/player.js
@@ -1,109 +1,79 @@
- function Player() {
-    this.frunzDiv = $("<div>").css({
-        position: "absolute",
-        width: "2vw",
-        height: "15vh",
-        border: "1px solid",
-        bottom: "60vh",
-        left:"1vw",
-    }).attr("id","player");
+function Player() {
+    this.frunzDiv = $("#player");
     console.log(this.frunzDiv.position().top);
     this.directions = {};
     this.speedX = 4;
     this.speedY = 0;
-    
+
     this.gravity = 0.15;
     this.jumpState = true;
 
-    this.walls = [$("#dontTouch"),$("#route")];
-    for(var i = 1;i<25;i++){
-        this.walls.push($(".window"+i))
+    this.walls = [$("#dontTouch"), $("#route")];
+    for (var i = 1; i < 25; i++) {
+        this.walls.push($(".window" + i))
     }
     this.STATIC_HORIZONTAL_CENTER;
     this.STATIC_VERTICAL_CENTER
-    
+
     this.WIDTH_DIFFERENCE;
     this.HEIGHT_DIFFERENCE;
-    
+
     this.DYNAMIC_HORIZONTAL_CENTER;
     this.DYNAMIC_VERTICAL_CENTER;
-    
+
     this.startPosition;
-    
 
-     this.charMovement= function (e)
-    {
-        directions[e.which] = true;
-    }
 
-   this.stop=function (e)
-    {
-        delete directions[e.which];
-    }
-    
-    this.move = function()
-    {
-        for (var i in this.directions) 
-        {
-            
-            if(this.frunzDiv.position().left > 0 && i == 37)
-            {
+    this.move = function () {
+        for (var i in this.directions) {
+
+            if (this.frunzDiv.position().left > 0 && i == 37) {
                 this.frunzDiv.css("left", (this.frunzDiv.position().left - this.speedX) + "px");
             }
-            
-            if(this.frunzDiv.position().left <($("#world").width() - this.frunzDiv.width()) && i == 39)
-            {
+
+            if (this.frunzDiv.position().left < ($("#world").width() - this.frunzDiv.width()) && i == 39) {
                 this.frunzDiv.css("left", (this.frunzDiv.position().left + this.speedX) + "px");
-            }   
-            
-            if(this.frunzDiv.position().top > 0 && i == 38 && this.jumpState == false)
-            {
-                
+            }
+
+            if (this.frunzDiv.position().top > 0 && i == 38 && this.jumpState == false) {
+
 
                 this.epsilon = 5;
                 this.jumpState = true;
                 console.log(this.frunzDiv.position().top);
-                if(this.frunzDiv.position().top<840)
-                {
-                    $("#world").css("top", ($("#world").position().top + this.speedY) + "px");
-                }
+
                 this.frunzDiv.css("top", this.frunzDiv.position().top - this.speedY + "px");
-                
-               
+
+
             }
         }
-        
+
         this.checkCollision();
         this.jump();
     }
-    
+
     this.epsilon = 5;
     console.log(this.walls[2].position().top)
-    this.checkCollision =function()
-    {   
-        for(var i in this.walls)
-        {
+    this.checkCollision = function () {
+        for (var i in this.walls) {
             //ONLY FLOOR
-            this.WIDTH_DIFFERENCE = this.frunzDiv.width()/2 + this.walls[i].width()/2;
-            this.STATIC_HORIZONTAL_CENTER = this.walls[i].position().left + this.walls[i].width()/2;
+            this.WIDTH_DIFFERENCE = this.frunzDiv.width() / 2 + this.walls[i].width() / 2;
+            this.STATIC_HORIZONTAL_CENTER = this.walls[i].position().left + this.walls[i].width() / 2;
             this.STATIC_VERTICAL_CENTER = this.walls[i].position().top;
-            
-           this.DYNAMIC_HORIZONTAL_CENTER = this.frunzDiv.position().left + this.frunzDiv.width()/2;
+
+            this.DYNAMIC_HORIZONTAL_CENTER = this.frunzDiv.position().left + this.frunzDiv.width() / 2;
             this.DYNAMIC_VERTICAL_CENTER = this.frunzDiv.position().top + this.frunzDiv.height();
-            
-            if( Math.abs(this.DYNAMIC_HORIZONTAL_CENTER - this.STATIC_HORIZONTAL_CENTER) < this.WIDTH_DIFFERENCE &&
-                Math.abs(this.DYNAMIC_VERTICAL_CENTER - this.STATIC_VERTICAL_CENTER) <= 0 + this.epsilon)
-            {
+
+            if (Math.abs(this.DYNAMIC_HORIZONTAL_CENTER - this.STATIC_HORIZONTAL_CENTER) < this.WIDTH_DIFFERENCE &&
+                Math.abs(this.DYNAMIC_VERTICAL_CENTER - this.STATIC_VERTICAL_CENTER) <= 0 + this.epsilon) {
                 this.epsilon = 0;
                 this.jumpState = false;
                 this.frunzDiv.css("top", this.walls[i].position().top - this.frunzDiv.height());
                 this.speedY = 9;
                 break;
             }
-            else
-            {
-                if(this.jumpState == false)
-                {
+            else {
+                if (this.jumpState == false) {
                     this.speedY = 0;
                     this.epsilon = 5;
                     this.jumpState = true;
@@ -112,15 +82,16 @@
             }
         }
     }
-    
 
-    
-    this.jump = function()
-    {
-        if(this.jumpState)
-        {
+
+
+    this.jump = function () {
+        if (this.jumpState) {
             this.speedY -= this.gravity;
             this.frunzDiv.css("top", this.frunzDiv.position().top - this.speedY);
+            if (this.frunzDiv.position().top < 840) {
+                $("#world").css("top", ($("#world").position().top + this.speedY) + "px");
+            }
         }
     }
 
--- script/script.js
+++ script/script.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
-    for(i = 1;i<=7;i++){
-        roof = $("<div/>").attr("class", "roof"+i+" "+" roof").prependTo("#building");
+    for (i = 1; i <= 7; i++) {
+        roof = $("<div/>").attr("class", "roof" + i + " " + " roof").prependTo("#building");
     }
-    
+
 
     var door = $("<div/>").attr("class", "door").appendTo(".roof1");
-    function wind(topPos,leftPos){
-    windowSill = $("<div>").css({
-        top: topPos,
-        left:leftPos,
-    }).attr("class", "window"+id+" "+"windowSill").appendTo("#world");
-    var windows = $("<div>").attr("id","windows").appendTo(windowSill);
+    function wind(topPos, leftPos) {
+        windowSill = $("<div>").css({
+            top: topPos,
+            left: leftPos,
+        }).attr("class", "window" + id + " " + "windowSill").appendTo("#world");
+        var windows = $("<div>").attr("id", "windows").appendTo(windowSill);
     }
-    var id=0;
-    for(var y = 11;y<=59;y+=16){
-        for(var i = 20;i<=157.5;i+=27.5){
+    var id = 0;
+    for (var y = 11; y <= 59; y += 16) {
+        for (var i = 20; i <= 157.5; i += 27.5) {
             id++;
-            wind(i+"vh",y+"vw");
+            wind(i + "vh", y + "vw");
         }
     }
-   
-    
+
+
     var player = new Player();
-    player.frunzDiv.appendTo("#world");
 
 
     $('html').keyup(stop).keydown(charMovement);
@@ -32,37 +31,16 @@ $(document).ready(function () {
     }
 
     function stop(e) {
-
-
         delete player.directions[e.which];
     }
 
     function render() {
-
         player.move();
-        update();
-        
+
     }
     var speed = 4;
-    function update(e) {
-        for (var i in player.directions) {
-            if (!player.directions.hasOwnProperty(i)) continue;
-
-            if (i == 38) {
-                
-                if (player.frunzDiv.position().top < 149) {
-                    player.frunzDiv.css("top", (player.frunzDiv.position().top - speed) + "px");
-                } else {
-                    $("#world").css("bottom", ($("#world").position().bottom -speed) + "px");
-                    
-
-                }
-
-            }
-        }
-    };
     var interval = setInterval(render, 15);
 
-    
+
 
 })
